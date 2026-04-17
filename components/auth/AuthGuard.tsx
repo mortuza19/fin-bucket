983074ceddbc8184e5351de64b205638e00cad5d
Sahil Mortuza <sahilmortuza.90@gmail.com>
2026-04-17 16:28:19 +0530

    Added proper logic in Auth guard

--- components/auth/AuthGuard.tsx
+++ components/auth/AuthGuard.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { useEffect, type ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { useAuth } from "@/lib/auth";
 import { Spinner } from "@/components/ui/spinner";
+import { createUserProfileIfNotExists } from "@/lib/firestore/users";
 
 export function AuthGuard({ children }: { children: ReactNode }) {
   const { user, loading } = useAuth();
@@ -11,7 +12,11 @@ export function AuthGuard({ children }: { children: ReactNode }) {
   const pathname = usePathname();
 
   const isPublicRoute = pathname === "/login";
-  const isRootRoute = pathname === "/";
+  const rootRoute = "/";
+  const onboardingRoute = "/onboarding";
+  const loginRoute = "/login";
+
+  const [checking, setChecking] = useState(true);
 
   useEffect(() => {
     if (loading) {
@@ -19,17 +24,33 @@ export function AuthGuard({ children }: { children: ReactNode }) {
     }
 
     if (!user && !isPublicRoute) {
-      router.replace("/login");
+      router.replace(loginRoute);
       return;
     }
 
     // TODO: Replace this logic later when we will have user DB.
-    if (user && (isPublicRoute || isRootRoute)) {
-      router.replace("/onboarding");
-    }
-  }, [loading, user, isPublicRoute, isRootRoute, router]);
+    if (user) {
+      const checkUser = async () => {
+        try {
+          const profile = await createUserProfileIfNotExists(user);
 
-  if (loading) {
+          if (!profile?.onboardingCompleted) {
+            router.replace(onboardingRoute);
+          } else {
+            router.replace(rootRoute);
+          }
+        } catch (err) {
+          console.error(err);
+          router.replace(loginRoute);
+        } finally {
+          setChecking(false);
+        }
+      };
+      checkUser();
+    }
+  }, [loading, user, isPublicRoute, router]);
+
+  if (loading || checking) {
     return (
       <div className="flex min-h-screen items-center justify-center bg-linear-to-br from-slate-50 via-blue-50 to-indigo-50">
         <Spinner className="size-10 text-black" />
